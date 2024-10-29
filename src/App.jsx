15f7c72f48ec3ed8f03a0d01356313b67920a74b
tyousofine <tyousofine@gmail.com>
2024-10-29 15:31:23 -0400

    info

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 
-import './App.css'
+
 
 function App() {
   const course = 'Half Stack application development'
@@ -10,19 +12,42 @@ function App() {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  const Part = () => {
+    return (
+      <div>
+        <p>{props.part1}, {props.exr1}</p>
+        <p>{props.part2}, {props.exr2}</p>
+        <p>{props.part3}, {props.exr3}</p>
+      </div>
+            )
+  }
+
+  return (
+    <Part part={props.part}/>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number exercises {props.exr1 + props.exr2 + props.exr3}</p>
+  )
+}
+
   return (
     <div>
-    <h1>{course}</h1>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3} exr1={exercises1} exr2={exercises2} exr3={exercises3}/>
+ 
+      <Total exr1={exercises1}
+      exr2={exercises2}
+      exr3={exercises3}/> 
   </div>
   )
 }
